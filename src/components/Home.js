698f9ea25e3919056f00c59131f0b9b14fc67c4f
Gagandeep <gagandeepkaur1523@gmail.com>
2022-10-30 19:33:38 +0530

    Made add and delete functional

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,29 @@
-import React from 'react';
-import Notes from './Notes';
+import React,{useState} from 'react';
 
 function Home() {
+
+  var [inputTodo, setInputTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const handleAddItem = (e)=>{
+      if(!inputTodo){
+
+      }
+      else{
+        // setTodoList(inputTodo) -> does not add in array but saves the string
+        setTodoList([...todoList, inputTodo]);
+        // ... are used to save the previous state ie data already in the array
+        setInputTodo=(''); 
+      }
+  }
+
+  const handleDelete = (i)=>{
+    const newTodoList = todoList.filter((el, index)=>{
+      return index !== i;
+    });
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className='d-flex flex-column flex-wrap align-content-center'>
       <div className='form'>
@@ -10,13 +32,28 @@ function Home() {
             <h2>Todo List</h2>
           </div>
           <div className="textField mt-4 mb-1 col-md-">
-            <label for="inputsm">Enter Todo</label>
-            <input type="text" class='form-control input-sm' />
+            <label htmlFor="inputsm">Enter Todo</label>
+            <input type="text" className='form-control input-sm' value={inputTodo} onChange={(e)=>setInputTodo(e.target.value)}/>
           </div>
-          <button type="button" class="btn btn-primary btn-sm">Add</button>
+          <button type="button" className="btn btn-primary btn-sm" onClick={handleAddItem}>Add</button>
         </form>
       </div>
-      <Notes />
+      <div className='todoList'>
+            {todoList.map((t,index) => {
+                return (
+                    <div className='container-sm'>
+                    <div className='card my-4'>
+                        <div className="card-body">
+                            <div className="d-flex">
+                                <p className="title mx-auto" key={index}>{t}</p>
+                                <button type="button" className="btn btn-outline-danger btn-sm" onClick={()=>handleDelete(index)}>Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                )
+            })}
+        </div>
     </div>
   )
 }
